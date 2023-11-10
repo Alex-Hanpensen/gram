360a4f2ca296e399727df27a4b7557336b72ae92
hanpensen <hanpensen90@mail.ru>
2023-11-10 14:42:39 +0300

    editing the utils file

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def read(path) -> FileNotFoundError | list[dict]:
-    path = os.path.join(BASE_DIR, fr"app\static\data{path}")
+    path = os.path.join(BASE_DIR, 'app', 'static', 'data', path)
     try:
         return json.load(open(path, 'r', encoding="utf8"))
     except FileNotFoundError:
@@ -13,16 +13,16 @@ def read(path) -> FileNotFoundError | list[dict]:
 
 
 def get_posts_all() -> list[dict]:
-    return read(r'\posts.json')
+    return read('posts.json')
 
 
 def get_posts_by_user(user_name: str) -> list[dict]:
-    data: list[dict] = read(r'\posts.json')
+    data: list[dict] = read('posts.json')
     return list(filter(lambda x: x['poster_name'] == user_name, data))
 
 
 def get_comments_by_post_id(post_id: int) -> ValueError | list:
-    data: list[dict] = read(r'\comments.json')
+    data: list[dict] = read('comments.json')
     comments = list(filter(lambda x: x['post_id'] == post_id, data))
     if comments:
         return comments
@@ -30,10 +30,10 @@ def get_comments_by_post_id(post_id: int) -> ValueError | list:
 
 
 def search_for_posts(query: str) -> list[dict]:
-    data: list[dict] = read(r'\posts.json')
+    data: list[dict] = read('posts.json')
     return [post for post in data if query in post['content']]
 
 
 def get_post_by_pk(pk: int) -> list[dict]:
-    data: list[dict] = read(r'\posts.json')
+    data: list[dict] = read('posts.json')
     return list(filter(lambda x: x['pk'] == pk, data))
